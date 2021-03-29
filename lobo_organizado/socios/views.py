e0001d9db3e986ecabb43b1ce7006f08f3ee0c8e
Claudio Dobniewski <claudio.dobniewski@intraway.com>
2021-03-28 23:50:47 -0300

    funciona familia listado y detalle
    Hay que sumarle estilo
    Hay que sumar en familias el crm_id , numero de familia (asignado), hay que llevar el valor en una funcion para autoasignar el siguiente disponible en una familia nueva.
    Iniciar la parte de cobranza

--- lobo_organizado/socios/views.py
+++ lobo_organizado/socios/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-from .models import Familia
+from django.http import Http404
+from .models import Familia, Socio
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the socios index.")
@@ -10,12 +11,15 @@ def familia_index(request):
 
     lista_familias = Familia.objects.all()
     output = ', '.join([fam.familia_crm_id for fam in lista_familias])
-    return HttpResponse(output)
+    print(lista_familias)
+    return render(request, 'socios/familia_listado.html', {'familias': lista_familias})
 
 def detalle(request, familia_id):
     
     try:
-        familia = Familia.objects.get(pk=familia_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'familia_detalle.html', {'familia': familia})
+        familia_socios = Familia.objects.get(pk=familia_id)
+        socios = Socio.objects.filter(familia_id=familia_socios.id)
+        print(socios)
+    except Exception as err:
+        raise Http404("Unexpected error: {0}".format(err))
+    return render(request, 'socios/familia_detalle.html', {'familia': familia_socios,'socios':socios})
