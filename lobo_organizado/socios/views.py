76d752522cd1218399acb5b6c28fd37ed55a0cc1
Claudio Dobniewski <claudio.dobniewski@intraway.com>
2021-03-30 23:40:29 -0300

    en familia detalle , gestion cobranza muestra cuotas y pagos basico

--- lobo_organizado/socios/views.py
+++ lobo_organizado/socios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
 from .models import Familia, Socio
-
+from cuotas.models import CuotaSocialFamilia,CuotaPago
 
 def index(request):
     return familias_index(request)
@@ -36,6 +36,15 @@ def familia_detalle(request, familia_id):
         familia_socios = Familia.objects.get(pk=familia_id)
         socios = Socio.objects.filter(familia_id=familia_socios.id)
         print(socios)
+
+        plan_de_pago = CuotaSocialFamilia.objects.filter(familia_id=familia_socios.id)
+       
+        print(plan_de_pago)
+        print("-------------")
+        pagos = CuotaPago.objects.filter(familia_id=familia_socios.id)
+        print(pagos)
+        print("-------------")
     except Exception as err:
         raise Http404("Unexpected error: {0}".format(err))
-    return render(request, 'socios/familia_detalle.html', {'familia': familia_socios,'socios':socios})
+    return render(request, 'socios/familia_detalle.html', {'familia': familia_socios,'socios':socios,'cuotas':plan_de_pago,'pagos':pagos})
+
