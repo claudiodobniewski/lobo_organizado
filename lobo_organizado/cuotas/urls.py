a7a17f95e02c589192643882371318d0190cec2c
Claudio Dobniewski <claudio.dobniewski@intraway.com>
2021-04-03 01:51:41 -0300

    update boton add,edit, new cuota, pago, obs

--- lobo_organizado/cuotas/urls.py
+++ lobo_organizado/cuotas/urls.py
@@ -8,5 +8,11 @@ app_name = 'cuotas'
 urlpatterns = [
     path('familia/nuevo_pago/<int:familia_id>', views.nuevo_pago, name='nuevo_pago'),
     path('familia/procesa_nuevo_pago/<int:familia_id>', views.procesa_nuevo_pago, name='procesa_nuevo_pago'),
+    path('familia/editar_pago/<int:pago_id>', views.editar_pago, name='editar_pago'),
+    path('familia/borrar_pago/<int:pago_id>', views.borrar_pago, name='borrar_pago'),
+    path('familia/nueva_cuota/<int:familia_id>', views.nueva_cuota, name='nueva_cuota'),
+    path('familia/procesa_nueva_cuota/<int:familia_id>', views.procesa_nueva_cuota, name='procesa_nueva_cuota'),
+    path('familia/editar_cuota/<int:cuota_id>', views.editar_cuota, name='editar_cuota'),
+    path('familia/borrar_cuota/<int:cuota_id>', views.borrar_cuota, name='borrar_cuota'),
     path('', views.index, name='index'),
 ]
--- lobo_organizado/socios/urls.py
+++ lobo_organizado/socios/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('socio/<int:socio_id>', views.socio_detalle, name='socio_detalle'),
     path('socios', views.socio_index, name='socio_listado'),
     path('familia/<int:familia_id>', views.familia_detalle, name='familia_detalle'),
+    path('familia/observacion/editar/<int:observacion_id>', views.familia_observacion_editar, name='familia_editar_observacion'),
+    path('familia/observacion/borrar/<int:observacion_id>', views.familia_observacion_borrar, name='familia_borrar_observacion'),
     path('familias', views.familia_index, name='familia_listado'),
 ]
