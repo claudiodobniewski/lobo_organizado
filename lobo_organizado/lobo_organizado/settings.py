9d2284a86b156626e9705e71bb1199f7185cd20f
Claudio Dobniewski <claudio.dobniewski@intraway.com>
2021-04-11 13:51:34 -0300

    Add favicon.ico

--- lobo_organizado/lobo_organizado/settings.py
+++ lobo_organizado/lobo_organizado/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/register'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,6 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
 
 # CLAUDIO LOGGING SECTION
 LOGGING = {
--- lobo_organizado/lobo_organizado/urls.py
+++ lobo_organizado/lobo_organizado/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 urlpatterns = [
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
     path('cuotas/', include('cuotas.urls')),
     path('socios/', include('socios.urls')),
     path('admin/', admin.site.urls),
