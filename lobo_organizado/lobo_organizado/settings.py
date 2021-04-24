e5b668d48db10a4b12823cbebc67f443acbbf5e0
Claudio Dobniewski <claudio.dobniewski@intraway.com>
2021-04-24 10:37:01 -0300

    locale, minor fixes
    listado familias, mensaje "no hay familias para listar" si no se encuentran familias
    se habuilita locale (traduccion/internacionalizacion) para poder modificar textos de validacion
    Familia familia_crm_id ahora es uniq
    Se suma para SOLO para usuario staff edicion familias crm_id y familia_crm_id

--- lobo_organizado/lobo_organizado/settings.py
+++ lobo_organizado/lobo_organizado/settings.py
@@ -132,6 +132,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+# https://devcode.la/tutoriales/internacionalizacion-en-django/
+LOCALE_PATHS = (
+ os.path.join(BASE_DIR, "locale"),
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- lobo_organizado/socios/models.py
+++ lobo_organizado/socios/models.py
@@ -6,7 +6,7 @@ from django.db import models
   
 class Familia(models.Model):
     
-    familia_crm_id = models.CharField(max_length=50)
+    familia_crm_id = models.CharField(max_length=50,unique=True)
     crm_id = models.IntegerField(unique=True) # unique=True
     direccion_calle = models.CharField(max_length=50,blank=True)
     direccion_numero = models.CharField(max_length=50,blank=True)
