c918e36c1d1e3ea9c3290e606e5de6a193a7c1df
Claudio Dobniewski <claudio.dobniewski@intraway.com>
2021-05-22 20:34:34 -0300

    update form y formato, aplica mejor limpieza filtros

--- lobo_organizado/cuotas/view_cobranzas.py
+++ lobo_organizado/cuotas/view_cobranzas.py
@@ -35,7 +35,7 @@ def absolute(value):
     else:
         return 0.0
 
-def gestion_cobranza_listado(request, error_message=''):
+def gestion_cobranza_listado(request, clean_filters=False, error_message=''):
 
     start_date = None
     end_date = None
@@ -45,7 +45,7 @@ def gestion_cobranza_listado(request, error_message=''):
 
      # BUSQUEDA
      
-    if request.method == 'GET': # If the form is submitted
+    if not clean_filters and request.method == 'GET': # If the form is submitted
         print("GET :{}".format(request.GET) )
         f_start_date=request.GET.get('f_start_date', None)
         f_end_date=request.GET.get('f_end_date', None)
@@ -66,14 +66,19 @@ def gestion_cobranza_listado(request, error_message=''):
         plan = f_plan
         if plan:
             lista_cuotas = lista_cuotas.filter(plan_de_pago=plan)
+    else:
+        print("NO FILTERS {}: {}".format(clean_filters,request.GET) )
+        f_start_date=''
+        f_end_date = date.today().strftime("%Y-%m-%d")
+        end_date = date.today()
+        f_plan=None
     
-    
-
+    print(date.today())
     print("GET:{} POST:{}  PLAN:{}  CUOTAS:{}".format(request.GET.get('f_start_date', None),request.POST.get('f_start_date', None),f_plan,lista_cuotas ))
     
 
     ###############################
-    lista_familias = Familia.objects.all().order_by('familia_crm_id')
+    lista_familias = Familia.objects.all().filter(eliminado=False).order_by('familia_crm_id')
     
     if plan:
         lista_planes = PlanDePago.objects.get(id=plan)
@@ -86,13 +91,18 @@ def gestion_cobranza_listado(request, error_message=''):
     for familia in lista_familias:
 
         # cuotas_todas,cuotas_por_plan,cuotas_vencidas,cuotas_suma,pagos_percibidos_queryset,pagos_percibidos_plan,pagos_percibidos_suma
+        ## TODO ver paginado si dejamos una pagina o N registros
+        ## TODO ver familias que no tienen deuda (un filtro mas, o paginas distintas?)
+        ## analizar el "start_date" si lo aplicamos
+        ## TODO default value para end_date today en el filtro.
 
         cuotas = app_cuotas.cuotas_queryset(familia.id)
         pagos =  app_cuotas.pagos_percibidos_queryset(familia.id)
         estado_plan = EstadoPlan()
-        estado_plan.familia = familia
+       
         if plan:
             print("Entro en 1 plan")
+            estado_plan.familia = familia
             estado_plan.plan_de_pago = lista_planes
             estado_plan.cuotas = app_cuotas.cuotas_por_plan(cuotas,lista_planes)
             estado_plan.cuotas_vencidas = app_cuotas.cuotas_vencidas(cuotas,end_date)
@@ -108,7 +118,19 @@ def gestion_cobranza_listado(request, error_message=''):
         else:
             print("Entro en varios planes")
             for un_plan in lista_planes:
+                estado_plan.familia = familia
                 estado_plan.plan_de_pago = app_cuotas.cuotas_por_plan(cuotas,un_plan)
+                estado_plan.plan_de_pago = un_plan
+                estado_plan.cuotas = app_cuotas.cuotas_por_plan(cuotas,un_plan)
+                estado_plan.cuotas_vencidas = app_cuotas.cuotas_vencidas(cuotas,end_date)
+                vencidas_importe = estado_plan.cuotas_vencidas_importe = app_cuotas.cuotas_suma(estado_plan.cuotas_vencidas)
+                estado_plan.pagos = app_cuotas.pagos_percibidos_plan(pagos, un_plan.id)
+                print(estado_plan.pagos )
+                pagos_importe = estado_plan.pagos_importe = app_cuotas.pagos_percibidos_suma(estado_plan.pagos)
+                balance_plan =  estado_plan.balance = vencidas_importe - float (pagos_importe)
+                estado_del_plan =  estado_plan.estado = 'OK' if balance_plan <= 0 else 'DEUDA'
+                print("FLIA:{} ESTADOD EL PLAN [{}] VDO:{} COB:{} BAL:{} EST:{}".format(familia,un_plan,vencidas_importe,pagos_importe,balance_plan,estado_del_plan))
+                reporte.append(estado_plan)
             reporte.append(estado_plan)
 
         #####
