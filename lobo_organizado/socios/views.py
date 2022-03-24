df2dc47bf0d81a40031cbb2752b51b941c63f12c
Claudio Dobniewski <claudio.dobniewski@intraway.com>
2022-03-23 21:08:34 -0300

    Cuando no hay resultados en la busqueda, se rompe al querer paginar sobre 0 registros.
    Garantizo al menos 1 pagina aun con cero registros.

--- lobo_organizado/socios/views.py
+++ lobo_organizado/socios/views.py
@@ -239,7 +239,7 @@ def familia_index(request,error_message=''):
     else:
         paginator = Paginator(lista_familias, 10) # Show x contacts per page.
     page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = paginator.get_page(page_number if page_number > 0 else 1)
 
     print("page_obj {}".format(page_obj))
 
